6a1a9911a4ea1840069a69f46fcbba2c66d13a00
Paweł Augustyniak <guustek@wp.pl>
2021-12-21 19:59:07 +0100

    ..

--- Game.js
+++ Game.js
@@ -1,31 +1,32 @@
 var config = {
     type: Phaser.AUTO,
-    parent: 'phaser-example',
     width: 800,
     height: 600,
     physics: {
-      default: 'arcade',
-      arcade: {
-        gravity: { y: 0 },
-        debug: false
-      }
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
     },
     scene: {
         preload: preload,
         create: create,
         update: update,
         extend: {
-                    player: null,
-                    healthpoints: null,
-                    reticle: null,
-                    moveKeys: null,
-                    playerBullets: null,
-                    enemyBullets: null,
-                    time: 0,
-                }
+            player: null,
+            healthpoints: null,
+            reticle: null,
+            moveKeys: null,
+            playerBullets: null,
+            enemyBullets: null,
+            time: 0,
+        }
     }
 };
 var game = new Phaser.Game(config);
+
+var hp = []
 
 var Bullet = new Phaser.Class({
 
@@ -33,34 +34,30 @@ var Bullet = new Phaser.Class({
 
     initialize:
 
-    // Bullet Constructor
-    function Bullet (scene)
-    {
-        Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
-        this.speed = 1;
-        this.born = 0;
-        this.direction = 0;
-        this.xSpeed = 0;
-        this.ySpeed = 0;
-        this.setSize(12, 12, true);
-    },
+        // Bullet Constructor
+        function Bullet(scene) {
+            Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
+            this.speed = 1;
+            this.born = 0;
+            this.direction = 0;
+            this.xSpeed = 0;
+            this.ySpeed = 0;
+            this.setSize(12, 12, true);
+        },
 
     // Fires a bullet from the player to the reticle
-    fire: function (shooter, target)
-    {
+    fire: function (shooter, target) {
         this.setPosition(shooter.x, shooter.y); // Initial position
-        this.direction = Math.atan( (target.x-this.x) / (target.y-this.y));
+        this.direction = Math.atan((target.x - this.x) / (target.y - this.y));
 
         // Calculate X and y velocity of bullet to moves it from shooter to target
-        if (target.y >= this.y)
-        {
-            this.xSpeed = this.speed*Math.sin(this.direction);
-            this.ySpeed = this.speed*Math.cos(this.direction);
+        if (target.y >= this.y) {
+            this.xSpeed = this.speed * Math.sin(this.direction);
+            this.ySpeed = this.speed * Math.cos(this.direction);
         }
-        else
-        {
-            this.xSpeed = -this.speed*Math.sin(this.direction);
-            this.ySpeed = -this.speed*Math.cos(this.direction);
+        else {
+            this.xSpeed = -this.speed * Math.sin(this.direction);
+            this.ySpeed = -this.speed * Math.cos(this.direction);
         }
 
         this.rotation = shooter.rotation; // angle bullet with shooters rotation
@@ -68,13 +65,11 @@ var Bullet = new Phaser.Class({
     },
 
     // Updates the position of the bullet each cycle
-    update: function (time, delta)
-    {
+    update: function (time, delta) {
         this.x += this.xSpeed * delta;
         this.y += this.ySpeed * delta;
         this.born += delta;
-        if (this.born > 1800)
-        {
+        if (this.born > 1800) {
             this.setActive(false);
             this.setVisible(false);
         }
@@ -82,52 +77,60 @@ var Bullet = new Phaser.Class({
 
 });
 
-function preload ()
-{
+function preload() {
     // Load in images and sprites
     this.load.image('player_handgun', 'assets/sprites/spaceship.png',
         { frameWidth: 66, frameHeight: 60 }
     );
+    this.load.image('enemy', 'assets/sprites/enemyShip.png', 
+     )
     this.load.image('bullet', 'assets/sprites/bullets/bullet1.png');
     this.load.image('target', 'assets/demoscene/ball.png');
-    this.load.image('background', 'assets/underwater1.png');
+    this.load.image('background', 'assets/background.gif');
+
+    this.load.image('health', 'assets/sprites/hp.png');
 }
 
-function create ()
-{
+function create() {
     // Set world bounds
-    this.physics.world.setBounds(0, 0, 1600, 1200);
+    this.physics.world.setBounds(0, 0, 920, 920);
 
     // Add 2 groups for Bullet objects
     playerBullets = this.physics.add.group({ classType: Bullet, runChildUpdate: true });
     enemyBullets = this.physics.add.group({ classType: Bullet, runChildUpdate: true });
 
     // Add background player, enemy, reticle, healthpoint sprites
-    var background = this.add.image(800, 600, 'background');
+    background = this.add.image(320, 320, 'background');
     player = this.physics.add.sprite(800, 600, 'player_handgun');
-    enemy = this.physics.add.sprite(300, 600, 'player_handgun');
+    enemy = this.physics.add.sprite(300, 600, 'enemy');
     reticle = this.physics.add.sprite(800, 700, 'target');
-    hp1 = this.add.image(-350, -250, 'target').setScrollFactor(0.5, 0.5);
-    hp2 = this.add.image(-300, -250, 'target').setScrollFactor(0.5, 0.5);
-    hp3 = this.add.image(-250, -250, 'target').setScrollFactor(0.5, 0.5);
-
-    // Set image/sprite properties
-    background.setOrigin(0.5, 0.5).setDisplaySize(1600, 1200);
-    player.setOrigin(0.5, 0.5).setDisplaySize(132, 120).setCollideWorldBounds(true).setDrag(500, 500);
-    enemy.setOrigin(0.5, 0.5).setDisplaySize(132, 120).setCollideWorldBounds(true);
-    reticle.setOrigin(0.5, 0.5).setDisplaySize(25, 25).setCollideWorldBounds(true);
-    hp1.setOrigin(0.5, 0.5).setDisplaySize(50, 50);
-    hp2.setOrigin(0.5, 0.5).setDisplaySize(50, 50);
-    hp3.setOrigin(0.5, 0.5).setDisplaySize(50, 50);
 
     // Set sprite variables
     player.health = 3;
     enemy.health = 3;
     enemy.lastFired = 0;
 
+    hp1 = this.add.image(-350, -250, 'health').setScrollFactor(0, 0);
+    hp2 = this.add.image(-300, -250, 'health').setScrollFactor(0, 0);
+    hp3 = this.add.image(-250, -250, 'health').setScrollFactor(0, 0);
+
+    // Set image/sprite properties
+    background.setOrigin(0.5, 0.5).setDisplaySize(2800, 2400);
+    player.setOrigin(0.5, 0.5).setDisplaySize(132, 120).setCollideWorldBounds(true).setDrag(500, 500);
+    enemy.setOrigin(0.5, 0.5).setDisplaySize(132, 120).setCollideWorldBounds(true);
+    reticle.setOrigin(0.5, 0.5).setDisplaySize(25, 25).setCollideWorldBounds(true);
+    for(let i = 0; i < hp.length; i++) {
+        hp[0].setOrigin(0.5, 0.5).setDisplaySize(50, 50);
+    }
+    hp1.setOrigin(0.5, 0.5).setDisplaySize(50, 50);
+    hp2.setOrigin(0.5, 0.5).setDisplaySize(50, 50);
+    hp3.setOrigin(0.5, 0.5).setDisplaySize(50, 50);
+
+
+
     // Set camera properties
     this.cameras.main.zoom = 0.5;
-    this.cameras.main.startFollow(player);
+    this.cameras.main.startFollow(reticle);
 
     // Creates object for input with WASD kets
     moveKeys = this.input.keyboard.addKeys({
@@ -177,8 +180,7 @@ function create ()
         // Get bullet from bullets group
         var bullet = playerBullets.get().setActive(true).setVisible(true);
 
-        if (bullet)
-        {
+        if (bullet) {
             bullet.fire(player, reticle);
             this.physics.add.collider(enemy, bullet, enemyHitCallback);
         }
@@ -197,8 +199,7 @@ function create ()
 
     // Move reticle upon locked pointer move
     this.input.on('pointermove', function (pointer) {
-        if (this.input.mouse.locked)
-        {
+        if (this.input.mouse.locked) {
             reticle.x += pointer.movementX;
             reticle.y += pointer.movementY;
         }
@@ -206,18 +207,15 @@ function create ()
 
 }
 
-function enemyHitCallback(enemyHit, bulletHit)
-{
+function enemyHitCallback(enemyHit, bulletHit) {
     // Reduce health of enemy
-    if (bulletHit.active === true && enemyHit.active === true)
-    {
+    if (bulletHit.active === true && enemyHit.active === true) {
         enemyHit.health = enemyHit.health - 1;
         console.log("Enemy hp: ", enemyHit.health);
 
         // Kill enemy if health <= 0
-        if (enemyHit.health <= 0)
-        {
-           enemyHit.setActive(false).setVisible(false);
+        if (enemyHit.health <= 0) {
+            enemyHit.setActive(false).setVisible(false);
         }
 
         // Destroy bullet
@@ -225,25 +223,22 @@ function enemyHitCallback(enemyHit, bulletHit)
     }
 }
 
-function playerHitCallback(playerHit, bulletHit)
-{
+function playerHitCallback(playerHit, bulletHit) {
     // Reduce health of player
-    if (bulletHit.active === true && playerHit.active === true)
-    {
+    if (bulletHit.active === true && playerHit.active === true) {
         playerHit.health = playerHit.health - 1;
         console.log("Player hp: ", playerHit.health);
 
+
+
         // Kill hp sprites and kill player if health <= 0
-        if (playerHit.health == 2)
-        {
+        if (playerHit.health == 2) {
             hp3.destroy();
         }
-        else if (playerHit.health == 1)
-        {
+        else if (playerHit.health == 1) {
             hp2.destroy();
         }
-        else
-        {
+        else {
             hp1.destroy();
             // Game over state should execute here
         }
@@ -253,22 +248,18 @@ function playerHitCallback(playerHit, bulletHit)
     }
 }
 
-function enemyFire(enemy, player, time, gameObject)
-{
-    if (enemy.active === false)
-    {
+function enemyFire(enemy, player, time, gameObject) {
+    if (enemy.active === false) {
         return;
     }
 
-    if ((time - enemy.lastFired) > 1000)
-    {
+    if ((time - enemy.lastFired) > 1000) {
         enemy.lastFired = time;
 
         // Get bullet from bullets group
         var bullet = enemyBullets.get().setActive(true).setVisible(true);
 
-        if (bullet)
-        {
+        if (bullet) {
             bullet.fire(enemy, player);
             // Add collider between bullet and player
             gameObject.physics.add.collider(player, bullet, playerHitCallback);
@@ -277,18 +268,16 @@ function enemyFire(enemy, player, time, gameObject)
 }
 
 // Ensures sprite speed doesnt exceed maxVelocity while update is called
-function constrainVelocity(sprite, maxVelocity)
-{
+function constrainVelocity(sprite, maxVelocity) {
     if (!sprite || !sprite.body)
-      return;
+        return;
 
     var angle, currVelocitySqr, vx, vy;
     vx = sprite.body.velocity.x;
     vy = sprite.body.velocity.y;
     currVelocitySqr = vx * vx + vy * vy;
 
-    if (currVelocitySqr > maxVelocity * maxVelocity)
-    {
+    if (currVelocitySqr > maxVelocity * maxVelocity) {
         angle = Math.atan2(vy, vx);
         vx = Math.cos(angle) * maxVelocity;
         vy = Math.sin(angle) * maxVelocity;
@@ -298,34 +287,36 @@ function constrainVelocity(sprite, maxVelocity)
 }
 
 // Ensures reticle does not move offscreen
-function constrainReticle(reticle)
-{
-    var distX = reticle.x-player.x; // X distance between player & reticle
-    var distY = reticle.y-player.y; // Y distance between player & reticle
+function constrainReticle(reticle) {
+    var distX = reticle.x - player.x; // X distance between player & reticle
+    var distY = reticle.y - player.y; // Y distance between player & reticle
 
     // Ensures reticle cannot be moved offscreen (player follow)
     if (distX > 800)
-        reticle.x = player.x+800;
+        reticle.x = player.x + 800;
     else if (distX < -800)
-        reticle.x = player.x-800;
+        reticle.x = player.x - 800;
 
     if (distY > 600)
-        reticle.y = player.y+600;
+        reticle.y = player.y + 600;
     else if (distY < -600)
-        reticle.y = player.y-600;
+        reticle.y = player.y - 600;
 }
 
-function update (time, delta)
-{
+function update(time, delta) {
     // Rotates player to face towards reticle
-    player.rotation = Phaser.Math.Angle.Between(player.x, player.y, reticle.x, reticle.y)+1.57;
+    player.rotation = Phaser.Math.Angle.Between(player.x, player.y, reticle.x, reticle.y) + 1.57;
 
     // Rotates enemy to face towards player
-    enemy.rotation = Phaser.Math.Angle.Between(enemy.x, enemy.y, player.x, player.y)+1.57;
+    enemy.rotation = Phaser.Math.Angle.Between(enemy.x, enemy.y, player.x, player.y) + 1.57;
+
+    // Camera follows reticle
+    this.cameras.main.startFollow(reticle);
 
     //Make reticle move with player
     reticle.body.velocity.x = player.body.velocity.x;
     reticle.body.velocity.y = player.body.velocity.y;
+
 
     // Constrain velocity of player
     constrainVelocity(player, 500);
